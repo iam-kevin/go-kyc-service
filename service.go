d3972f06bf011925149e3962e8f85a0a16a5f68a
Kevin James <54111777+iam-kevin@users.noreply.github.com>
2023-12-02 05:27:08 +0300

    build: setup simple service

---
+++ service.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func GetHandler(w *http.ResponseWriter, r *http.Request) {
+}
+
+func PostHandler(w *http.ResponseWriter, r *http.Request) {
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		default:
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			fmt.Fprint(w, "{ \"message\": \"not sure we support that buddy\"}")
+			return
+		case "POST":
+			PostHandler(&w, r)
+			return
+		case "GET":
+			GetHandler(&w, r)
+			return
+		}
+	})
+
+	http.ListenAndServe(":3455", r)
+}
